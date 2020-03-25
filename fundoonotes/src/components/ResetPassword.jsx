92e208c80d251ce5b6529158deaf47d420388d91
GowriRajeshwari <gowrikanaga216@gmail.com>
2020-03-25 11:13:57 +0530

    [GowriK] Refactor : Changed the reset password class name

--- fundoonotes/src/components/ResetPassword.jsx
+++ fundoonotes/src/components/ResetPassword.jsx
@@ -5,7 +5,7 @@ import TextField from "@material-ui/core/TextField";
 import profile from '../assets/profile.png';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
-class Dashboard extends Component {
+class ResetPassword extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -113,4 +113,4 @@ class Dashboard extends Component {
     );
   }
 }
-export default Dashboard;
+export default ResetPassword;
