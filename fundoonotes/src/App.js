f5a9688780a1f30398f7690bd7ced94a911f31f7
GowriRajeshwari <gowrikanaga216@gmail.com>
2020-03-25 11:24:06 +0530

    [GowriK] Refactor : Added the dashbord file in app

--- fundoonotes/src/App.js
+++ fundoonotes/src/App.js
@@ -6,6 +6,8 @@ import Login from './components/Login';
 import Register from './components/Register';
 import ForgotPassword from './components/ForgotPassword';
 import ResetPassword from './components/ResetPassword';
+import Dashboard from './components/Dashboard';
+
 
 
 
@@ -13,10 +15,11 @@ function App() {
   return (
     <Router history={history}>
       <Switch>
-        <Route path="/" exact component={Login} />
+        {/* <Route path="/" exact component={Login} />
         <Route path="/Register" component={Register} />
         <Route path="/Forgot" component={ForgotPassword} />
-        <Route path="/Reset" component={ResetPassword} />
+        <Route path="/Reset" component={ResetPassword} /> */}
+        <Route path="/Dashboard" component={Dashboard} />
       </Switch>
     </Router>
   );
---
+++ fundoonotes/src/components/Dashboard.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import mail from '../assets/mail.png';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+
+class Dashboard extends Component {
+
+ 
+    render() {
+        return (
+         <div>
+             hello
+         </div>  
+        );
+    }
+}
+export default Dashboard;
