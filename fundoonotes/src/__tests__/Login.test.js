1a1a94757bc61347e8c97aaeb71c033d9f01e640
GowriRajeshwari <gowrikanaga216@gmail.com>
2020-05-05 12:53:54 +0530

    [GowriK] Refactor : Testing for Email and password textfield is done

--- fundoonotes/src/__tests__/Login.test.js
+++ fundoonotes/src/__tests__/Login.test.js
@@ -10,29 +10,37 @@ import Login from '../components/Login';
 
 configure({adapter: new Adapter()});
 
-describe('Login component', () => {
-  it('email Input',()=>{
+describe('Test case for testing login component', () => {
+
+  it('Email Textfield',()=>{
       expect(shallow(<Login/>).find("#btnEmail").length).toEqual(1)
   })
+  it('Password Textfield',()=>{
+    expect(shallow(<Login/>).find("#btnPassword").length).toEqual(1)
+})
+
+describe('username check',()=>{
+    it('input of email',()=>{
+        const wrapper = shallow(<Login/>);
+        wrapper.find("#btnEmail").simulate('change',{target : { name:'Emails', value : 'gowripanda35@gmail.com'}
+        });
+        expect(wrapper.state('email')).toEqual('gowripanda35@gmail.com')
+    })
+})
+describe('Password Check',()=>{
+    it('input of password',()=>{
+        const wrapper = shallow(<Login/>);
+        wrapper.find("#btnPassword").simulate('change',{target : { name:'Password', value : 'Gowri@35'}
+        });
+        expect(wrapper.state('password')).toEqual('Gowri@35')
+    })
+})
+// describe('SignIn Button Check',()=>{
 //   it('SignIn Button', () => {
 //     const wrapper = shallow(<Login />);
 //     const decrementBtn = wrapper.find('.SignIn').at(0);
 //     decrementBtn.simulate('click');
 //     expect(true).toBe(true)
 //   });
-describe('email Input',()=>{
-    it('input of email',()=>{
-        const wrapper = shallow(<Login/>);
-        wrapper.find("#btnEmail")
-        .simulate('change',{
-            target : {
-                name:'Emails',
-                value : 'gowripanda35@gmail.com'
-            }
-        });
-      
-        expect(wrapper.state('email')).toEqual('gowripanda35@gmail.com')
-    })
-})
- 
+// })
 });
